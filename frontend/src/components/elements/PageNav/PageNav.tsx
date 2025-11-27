1b49b6c7544c8d42e0e206b18224f5d241d78eaa
Alexandr <volokitin200@bk.ru>
2025-11-27 20:02:00 +0300

    client: renamed field

--- frontend/src/components/elements/PageNav/PageNav.tsx
+++ frontend/src/components/elements/PageNav/PageNav.tsx
@@ -4,20 +4,20 @@ interface PageNavProps {
   page: number;
   size: number;
   onPageChange: (page: number) => void;
-  ticketsAmount: number
+  entitiesAmount: number
 }
 
 export const PageNav = ({
   page,
   size,
   onPageChange,
-  ticketsAmount
+  entitiesAmount
 }: PageNavProps) => {
   return (
     <div className="page-nav-container">
         <button className="page-nav-button" onClick={() => onPageChange(page - 1)}>Back</button>
       
-      <div className="page-nav-numbers">Page {page + 1} ({Math.min(size * page + 1, ticketsAmount)} - {Math.min(size * (page + 1), ticketsAmount)} of {ticketsAmount})</div>
+      <div className="page-nav-numbers">Page {page + 1} ({Math.min(size * page + 1, entitiesAmount)} - {Math.min(size * (page + 1), entitiesAmount)} of {entitiesAmount})</div>
       <button className="page-nav-button" onClick={() => onPageChange(page + 1)}>Next</button>
       
     </div>
